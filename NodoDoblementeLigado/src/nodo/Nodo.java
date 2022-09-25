42de335489bde00208fd8f483d337cdeb54fbbea
Victor25 <victor1203rmmtz@gmail.com>
2022-09-25 01:29:03 -0500

    Nodo Doble Mente Ligada 25/09 01:28

--- NodoDoblementeLigado/src/nodo/Nodo.java
+++ NodoDoblementeLigado/src/nodo/Nodo.java
@@ -1,13 +1,61 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package nodo;
 
 /**
  *
  * @author victo
  */
-public class Nodo {
-    
+public class Nodo<T> {
+
+    private T head;
+    private Nodo<T> siguiente;
+    private Nodo<T> anterior;
+
+    public Nodo() {
+    }
+
+    /*ESTE CONSTRUCTOR VA A SERVIR PARA INICIALIZAR LA LISTA, ES CUANDO NO HAY
+    DATOS
+     */
+    public Nodo(T head) {
+        this.head = head;
+    }
+
+    /*ESTE CONSTUCTOR ES CUANDO YA HAY DATOS EN LA LISTA
+     */
+    public Nodo(T head, Nodo<T> siguiente, Nodo<T> anterior) {
+        this.head = head;
+        this.siguiente = siguiente;
+        this.anterior = anterior;
+    }
+
+    //CREAMOS LOS METOS DE ACCESO PARA MOSTRAR Y MODIFICAR CADA ATRIBUTO
+    public Nodo<T> getAnterior() {
+        return anterior;
+    }
+
+    public void setAnterior(Nodo<T> anterior) {
+        this.anterior = anterior;
+    }
+
+    public T getHead() {
+        return head;
+    }
+
+    public void setHead(T head) {
+        this.head = head;
+    }
+
+    public Nodo<T> getSiguiente() {
+        return siguiente;
+    }
+
+    public void setSiguiente(Nodo<T> siguiente) {
+        this.siguiente = siguiente;
+    }
+
+    @Override
+    public String toString() {
+        return "|" + head + "| -->";
+    }
+
 }
---
+++ NodoDoblementeLigado/src/nodo/NodoMain.java
@@ -0,0 +1,18 @@
+package nodo;
+
+/**
+ *
+ * @author victo
+ */
+public class NodoMain {
+
+    public static void main(String[] args) {
+
+        Nodo<Integer> cabeza = new Nodo<>(50);
+
+        System.out.println(cabeza);
+        
+        
+
+    }
+}
