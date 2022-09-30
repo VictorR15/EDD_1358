5fa8bc63d277ee8476222eb86f0a1075d4450b54
VictorR15 <victor1203rmmtz@gmail.com>
2022-09-30 11:51:07 -0500

    Suma de vectores

---
+++ sumaDeVectoresEyM/sumaDeVectoresEyM/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sumaDeVectoresEyM
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numeroDeVectores;
+            double grados, magnitud;
+            
+            
+            Console.WriteLine("Suma de vectores");
+
+            Console.WriteLine("¿Cuantos vectores quieres sumar?");
+            numeroDeVectores = Convert.ToInt32(Console.ReadLine());
+            
+
+            
+
+
+
+        }
+    }
+}
