fbb7b5e1b51fba477684d03c02d9d07050ae866e
Victor25 <victor1203rmmtz@gmail.com>
2022-10-01 01:07:50 -0500

    Avance

--- sumaDeVectoresEyM/sumaDeVectoresEyM/Program.cs
+++ sumaDeVectoresEyM/sumaDeVectoresEyM/Program.cs
@@ -12,13 +12,19 @@ namespace sumaDeVectoresEyM
         static void Main(string[] args)
         {
             int numeroDeVectores;
-            double grados, magnitud;
+            double gradosVec1, gradosVec2, magnitudVec1,magnitudVec2;
             
             
             Console.WriteLine("Suma de vectores");
 
-            Console.WriteLine("¿Cuantos vectores quieres sumar?");
-            numeroDeVectores = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("¿Cuantos vectores quieres sumar?");
+            //numeroDeVectores = Convert.ToInt32(Console.ReadLine());
+
+
+
+            
+            
+
             
 
             
