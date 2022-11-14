fc9bdd40cc9b11088d77d77a0aec2757e9b91811
VictorR15 <victor1203rmmtz@gmail.com>
2022-11-14 12:58:41 -0600

    Tabla

--- ColaDePrioridad/src/cola/Cola.java
+++ ColaDePrioridad/src/cola/Cola.java
@@ -4,9 +4,10 @@ package cola;
  *
  * @author victo
  */
-public class Cola <T>{
+public class Cola<T> {
 
     Nodo<T>[] datoCola;
+    public static int numeroElementos;
 
     public Cola() {
     }
@@ -14,21 +15,25 @@ public class Cola <T>{
     public Cola(Nodo<T>[] datoCola) {
         this.datoCola = datoCola;
     }
-    
-    public boolean is_empty(){
-        
+
+    public boolean is_empty() {
+
         return this.datoCola == null;
     }
-    
-    
-    
-    
 
+    public int length() {
+        
+        return numeroElementos;
+
+    }
     
-    
-    
-    
- 
-    
-    
+    public void enqueue( int prioridad, Nodo elemento){
+        
+        Nodo nuevoDato = new Nodo(elemento);
+        
+        this.datoCola = nuevoDato;
+        numeroElementos++;
+        
+    }
+
 }
